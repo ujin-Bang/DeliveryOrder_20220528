1c04fa2d7c99702555458d49ba2d2ae79b1155f5
ujin-Bang <qkddnwls14@gmail.com>
2022-05-28 21:43:31 +0900

    상세보기 화면 받아온 실제 데이터 입력  - putExtra에서 보낸 Serializable형태로 받아 as 로 StoreData 형변환   - Glide활용

--- app/src/main/java/com/start/deliveryorder_20220528/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/start/deliveryorder_20220528/ViewStoreDetailActivity.kt
@@ -3,8 +3,10 @@ package com.start.deliveryorder_20220528
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.start.deliveryorder_20220528.databinding.ActivityMainBinding
 import com.start.deliveryorder_20220528.databinding.ActivityViewStoreDetailBinding
+import com.start.deliveryorder_20220528.datas.StoreData
 
 class ViewStoreDetailActivity : AppCompatActivity() {
 
@@ -22,6 +24,9 @@ class ViewStoreDetailActivity : AppCompatActivity() {
     }
 
     fun setValues(){
-
+       val storeData =  intent.getSerializableExtra("store") as StoreData
+        Glide.with(this).load(storeData.logoURL).into(binding.imgLogo)
+        binding.txtStoreName.text = storeData.name
+        binding.txtPhoneNum.text = storeData.phoneNum
     }
 }
