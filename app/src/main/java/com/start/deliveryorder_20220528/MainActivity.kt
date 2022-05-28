a3fd721decb2f2552a8e544c622bfec46ea7d961
ujin-Bang <qkddnwls14@gmail.com>
2022-05-28 19:19:44 +0900

    ViewPager 어댑터를 객체화 -> 화면의 ViewPager와 연결

--- app/src/main/java/com/start/deliveryorder_20220528/MainActivity.kt
+++ app/src/main/java/com/start/deliveryorder_20220528/MainActivity.kt
@@ -3,14 +3,29 @@ package com.start.deliveryorder_20220528
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.start.deliveryorder_20220528.adapters.MainViewPagerAdapter
 import com.start.deliveryorder_20220528.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var mvpa: MainViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents(){
+
+    }
+
+    fun setValues(){
+
+        mvpa = MainViewPagerAdapter(supportFragmentManager)
+        binding.mainViewPager.adapter = mvpa
+
     }
 }
