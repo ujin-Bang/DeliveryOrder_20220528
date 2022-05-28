23427a7a53b5242612ad2d67b42be8dc2ceabe82
ujin-Bang <qkddnwls14@gmail.com>
2022-05-28 19:49:04 +0900

    피자가게 어댑터 기초 작업 마무리

--- app/src/main/java/com/start/deliveryorder_20220528/adapters/PizzaStoreAdapter.kt
+++ app/src/main/java/com/start/deliveryorder_20220528/adapters/PizzaStoreAdapter.kt
@@ -1,12 +1,28 @@
 package com.start.deliveryorder_20220528.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.start.deliveryorder_20220528.R
 import com.start.deliveryorder_20220528.datas.StoreData
 
 class PizzaStoreAdapter(
     val mContext: Context,
-    val resId: Int,
+    resId: Int,
     val mList: ArrayList<StoreData>): ArrayAdapter<StoreData>(mContext, resId, mList) {
 
+        val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if ( tempRow == null ) {
+            tempRow = mInflater.inflate(R.layout.store_list_item, null)
+        }
+        val row = tempRow!!
+
+        return row
+    }
 }
