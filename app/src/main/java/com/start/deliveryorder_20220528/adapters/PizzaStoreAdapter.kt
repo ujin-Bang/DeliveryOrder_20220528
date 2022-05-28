1cd8311c1008d6e353ca83a7039dfaf923b689b2
ujin-Bang <qkddnwls14@gmail.com>
2022-05-28 20:09:55 +0900

    실제 리스트 데이터 반영  - Glide 활용

--- app/src/main/java/com/start/deliveryorder_20220528/adapters/PizzaStoreAdapter.kt
+++ app/src/main/java/com/start/deliveryorder_20220528/adapters/PizzaStoreAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.start.deliveryorder_20220528.R
 import com.start.deliveryorder_20220528.datas.StoreData
 
@@ -30,7 +31,8 @@ class PizzaStoreAdapter(
         val imgLogo = row.findViewById<ImageView>(R.id.imgLogo)
         val txtStoreName = row.findViewById<TextView>(R.id.txtStoreName)
 
-
+        txtStoreName.text = data.name
+        Glide.with(mContext).load(data.logoURL).into(imgLogo)
 
 
         return row
