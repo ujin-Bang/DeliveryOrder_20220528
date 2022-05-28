7a9c1cce8a7cb807872a54fd5e73c868aab2ad74
ujin-Bang <qkddnwls14@gmail.com>
2022-05-28 18:52:52 +0900

    메인  - 바인딩 연결 / 탭레이아웃 / 뷰페이저 배치

--- app/src/main/java/com/start/deliveryorder_20220528/MainActivity.kt
+++ app/src/main/java/com/start/deliveryorder_20220528/MainActivity.kt
@@ -2,10 +2,15 @@ package com.start.deliveryorder_20220528
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.start.deliveryorder_20220528.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
