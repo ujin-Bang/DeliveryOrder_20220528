a1dbd333ebfbc809313ab84ddefc77ddfca3b521
ujin-Bang <qkddnwls14@gmail.com>
2022-05-28 19:23:24 +0900

    페이지별 제목 설정

--- app/src/main/java/com/start/deliveryorder_20220528/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/start/deliveryorder_20220528/adapters/MainViewPagerAdapter.kt
@@ -7,6 +7,14 @@ import com.start.deliveryorder_20220528.fragments.MyProfileFragment
 import com.start.deliveryorder_20220528.fragments.PizzaStoreListFragment
 
 class MainViewPagerAdapter(fm: FragmentManager): FragmentPagerAdapter(fm) {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position){
+            0 -> "피자 주문"
+            else -> "내 정보 설정"
+        }
+    }
+
     override fun getCount(): Int {
         return 2
     }
