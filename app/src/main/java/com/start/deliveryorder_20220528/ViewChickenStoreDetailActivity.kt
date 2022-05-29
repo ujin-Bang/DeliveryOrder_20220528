fdca9e2b36640c9d597cd542dbbbecdd29cbcb07
ujin-Bang <qkddnwls14@gmail.com>
2022-05-29 16:38:17 +0900

    주문하기 버튼 클릭시 -> 권한설정 -> 전화연결  - TedPermissin  - 권한 허용시 -전화연결 intent(3) : Uri.parse("tel:${...}")  - 권한 불가시 - 토스트 띄우기

--- app/src/main/java/com/start/deliveryorder_20220528/ViewChickenStoreDetailActivity.kt
+++ app/src/main/java/com/start/deliveryorder_20220528/ViewChickenStoreDetailActivity.kt
@@ -1,9 +1,15 @@
 package com.start.deliveryorder_20220528
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.start.deliveryorder_20220528.databinding.ActivityViewChickenStoreDetailBinding
 import com.start.deliveryorder_20220528.datas.ChickenStoreData
 
@@ -21,6 +27,30 @@ class ViewChickenStoreDetailActivity : AppCompatActivity() {
     }
 
     fun setupEvents(){
+
+        binding.btnCall.setOnClickListener {
+
+            val permission = object : PermissionListener {
+                override fun onPermissionGranted() {
+
+                    val myUri = Uri.parse("tel:${mChickenStoreData.phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                    Toast.makeText(this@ViewChickenStoreDetailActivity, "권한 허용불가로 통화연결이 되지않습니다.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+            }
+            TedPermission.create()
+                .setPermissionListener(permission)
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
+        }
 
     }
 
