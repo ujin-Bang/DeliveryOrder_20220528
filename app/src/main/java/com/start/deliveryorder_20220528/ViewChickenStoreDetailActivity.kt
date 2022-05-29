9e825d1d6bb090758763e1c3959a10fa16de8b67
ujin-Bang <qkddnwls14@gmail.com>
2022-05-29 16:31:20 +0900

    주문 상세페이지 실제데이터 입력  - 치킨스토어 데이터를 여러곳에서 쓸 수 있게 lateinit var로 만들어 둠   - getSerializableExtra("storeData") as ChickenStoreData   - Glide

--- app/src/main/java/com/start/deliveryorder_20220528/ViewChickenStoreDetailActivity.kt
+++ app/src/main/java/com/start/deliveryorder_20220528/ViewChickenStoreDetailActivity.kt
@@ -3,11 +3,14 @@ package com.start.deliveryorder_20220528
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.start.deliveryorder_20220528.databinding.ActivityViewChickenStoreDetailBinding
+import com.start.deliveryorder_20220528.datas.ChickenStoreData
 
 class ViewChickenStoreDetailActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityViewChickenStoreDetailBinding
+    lateinit var mChickenStoreData : ChickenStoreData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +25,10 @@ class ViewChickenStoreDetailActivity : AppCompatActivity() {
     }
 
     fun setValues(){
+        mChickenStoreData = intent.getSerializableExtra("storeData") as ChickenStoreData
+        binding.txtStoreName.text = mChickenStoreData.storeName
+        binding.txtPhoneNum.text = mChickenStoreData.phoneNum
+        Glide.with(this).load(mChickenStoreData.logoURL).into(binding.imgLogo)
 
     }
 }
